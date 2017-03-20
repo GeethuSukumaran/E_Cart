6bd661317b844001080f80c7ff56c6582d106da0
GeethuSukumaran <geeesub@gmail.com>
2017-03-20 15:59:02 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ECart::Application.config.secret_key_base = 'a9c0d6175fbbe092cc0bdf8efbeed41b529e1adf06c6c86d3fdb7759e2f2b35c49542247585a50db995702cb22d779f3e548205fe5d91e566e9390be8ca03199'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ECart::Application.config.session_store :cookie_store, key: '_E_Cart_session'
